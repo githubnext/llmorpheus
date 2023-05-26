751d7241a62929073667a5f7ed11ed5fe1f6d8f7
franktip <f.tip@northeastern.edu>
2023-05-26 09:48:47 -0400

    allow model selection and caching from command line

--- benchmark/createMutants.ts
+++ benchmark/createMutants.ts
@@ -38,7 +38,17 @@ if (require.main === module) {
           type: "number",
           default: 5,
           description: "number of completions to generate for each prompt (default: 5)",
-        }
+        },
+        model: {
+          type: "string",
+          default: "text-davinci003",
+          description: "name of the model to use (default: \"text-davinci003\")",
+        },
+        caching: {
+          type: "boolean",
+          default: true,
+          description: "whether to cache the results of queries to the model (default: true)",
+        },
     });
     const argv = await parser.argv;
     const rules = argv.rules === undefined ? [] : argv.rules!.substring(1, argv.rules!.length-1).split(",");
@@ -48,7 +58,22 @@ if (require.main === module) {
       return argv.rules === undefined || rules.includes(value);
     }
 
-    const model = new CachingModel(new Gpt35TurboModel({ max_tokens: 500, stop: ["DONE"], temperature: 0.0, n: argv.numCompletions }));
+    if (argv.model !== "text-davinci003" && argv.model !== "gpt3.5-turbo") {
+      console.error(`Invalid model name: ${argv.model}`);
+      process.exit(1);
+    }
+
+    let baseModel, model;
+    if (argv.model === "text-davinci003") {
+      baseModel = new TextDavinci003Model({ max_tokens: 500, stop: ["DONE"], temperature: 0.0, n: argv.numCompletions });
+    } else {
+      baseModel = new Gpt35TurboModel({ max_tokens: 500, stop: ["DONE"], temperature: 0.0, n: argv.numCompletions });
+    }
+    if (argv.caching) {
+      model = new CachingModel(baseModel);
+    } else {
+      model = baseModel;
+    }
 
     const mutantGenerator = new MutantGenerator(model, argv.promptTemplateFileName, argv.rulesFileName, ruleFilter, argv.outputDir);
     mutantGenerator.generateMutants(argv.path);
