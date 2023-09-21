0f4af23c10f0764514bf51fef7992b38dff2326a
franktip <f.tip@northeastern.edu>
2023-09-21 16:12:56 -0400

    update report generation

---
+++ benchmark/generateReport.ts
@@ -0,0 +1,27 @@
+import * as fs from "fs";
+
+// read dirName from command line
+const dirName = process.argv[2];
+const title = "Mutant Generation Report";
+
+let report = `
+# ${title}
+Project | # NrCandidates | # NrSyntacticallyInvalid | # NrIdentical | # NrDuplicate |  # NrSyntacticallyValid | # NrLocations 
+--- |  --: | --: | --: | --: | --: |`;
+
+const results = fs.readdirSync(dirName);
+for (const benchmarkName of results){
+  if (benchmarkName.startsWith(".")) continue;
+  // read summary.json file in benchmarkName/MUTATION_TESTING
+  const summary = JSON.parse(fs.readFileSync(`${dirName}/${benchmarkName}/MUTATION_TESTING/summary.json`, "utf8"));
+  const nrCandidates = summary.nrCandidates;
+  const nrSyntacticallyInvalid = summary.nrSyntacticallyInvalid;
+  const nrSyntacticallyValid = summary.nrSyntacticallyValid;
+  const nrIdentical = summary.nrIdentical;
+  const nrDuplicate = summary.nrDuplicate;
+  const nrLocations = summary.nrLocations;
+  report += `
+${benchmarkName} | ${nrCandidates} | ${nrSyntacticallyInvalid} | ${nrIdentical} | ${nrDuplicate} | ${nrSyntacticallyValid} | ${nrLocations}`;
+  // console.log(`${benchmarkName},${nrCandidates},${nrSyntacticallyInvalid},${nrSyntacticallyValid},${nrIdentical},${nrDuplicate},${nrLocations}`);
+}
+console.log(report);
