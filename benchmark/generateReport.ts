4ef32a2c839af414c44c187617f1b7563bf5416d
franktip <f.tip@northeastern.edu>
2023-09-22 14:50:43 -0400

    update report generation

--- benchmark/generateReport.ts
+++ benchmark/generateReport.ts
@@ -4,19 +4,19 @@ import * as fs from "fs";
 const dirName = process.argv[2];
 const title = "Mutant Generation Report";
 
-let report = `
+console.log(`
 # ${title}
 Project | # NrCandidates | # NrSyntacticallyInvalid | # NrIdentical | # NrDuplicate |  # NrSyntacticallyValid | # NrLocations 
---- |  --: | --: | --: | --: | --: |`;
+--- |  --: | --: | --: | --: | --: |`);
 
-console.log(`dirName: ${dirName}, type: ${typeof dirName}`);
+// console.log(`dirName: ${dirName}, type: ${typeof dirName}`);
 const results = fs.readdirSync(dirName);
 for (const benchmarkName of results){
   if (benchmarkName.startsWith(".")) continue;
-  console.log(`benchmarkName: ${benchmarkName}, type: ${typeof benchmarkName}`);
+  // console.log(`benchmarkName: ${benchmarkName}, type: ${typeof benchmarkName}`);
   // read summary.json file in benchmarkName/MUTATION_TESTING
   const file = fs.readFileSync(`${dirName}/${benchmarkName}/MUTATION_TESTING/summary.json`, "utf8");
-  console.log(`file: ${file}, type: ${typeof file}`);
+  // console.log(`file: ${file}, type: ${typeof file}`);
   const summary = JSON.parse(file);
   const nrCandidates = summary.nrCandidates;
   const nrSyntacticallyInvalid = summary.nrSyntacticallyInvalid;
@@ -24,8 +24,6 @@ for (const benchmarkName of results){
   const nrIdentical = summary.nrIdentical;
   const nrDuplicate = summary.nrDuplicate;
   const nrLocations = summary.nrLocations;
-  report += `
-${benchmarkName} | ${nrCandidates} | ${nrSyntacticallyInvalid} | ${nrIdentical} | ${nrDuplicate} | ${nrSyntacticallyValid} | ${nrLocations}`;
-  // console.log(`${benchmarkName},${nrCandidates},${nrSyntacticallyInvalid},${nrSyntacticallyValid},${nrIdentical},${nrDuplicate},${nrLocations}`);
+  console.log(
+    `${benchmarkName} | ${nrCandidates} | ${nrSyntacticallyInvalid} | ${nrIdentical} | ${nrDuplicate} | ${nrSyntacticallyValid} | ${nrLocations}`);
 }
-console.log(report);
