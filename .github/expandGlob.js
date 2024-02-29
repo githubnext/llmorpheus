b0e55455de20cb16c1418f986de320a4d291eae7
franktip <f.tip@northeastern.edu>
2024-02-29 15:59:34 -0500

    fix workflow

--- .github/expandGlob.js
+++ .github/expandGlob.js
@@ -1,37 +1,10 @@
 const fg = require('fast-glob');
 
 async function expandGlob(pattern, packagePath) {
-  const files = await fg([pattern], {
-    dot: true,
-    ignore: ['**/node_modules',
-      '**/dist',
-      '**/test',
-      '**/*.test.*',
-      '**/*.min.js',
-      '**/*.d.ts',
-      '**/rollup.config.js',
-      "**/esm/index.js",
-      'coverage',
-      'lcov-report',
-      `${packagePath}/**/*test*.js`,
-      '**/examples',
-      '**/example',
-      '**/benchmark',
-      '**/benchmarks',
-      "**/*.spec.*",
-      '**/build',
-      '**/test.js',
-      '**/Gruntfile.js',
-      '**/design/**',
-      '**/spec/**',
-      '**/scripts/**',]
-  });
-  const filesWithoutProjectPath = files.map((file) => file.replace(packagePath, ""));
-  console.log(filesWithoutProjectPath.join(','));}
+  let files = (await fg([pattern], { })).map((file) => file.replace(packagePath, "./"));
+  console.log(files.join(','));
+}
 
 const packagePath = process.argv[2].trim();
 const glob = process.argv[3].trim();
-// console.log(`packagePath = ${packagePath}`);
-// console.log(`glob = ${glob}`);
-
 expandGlob(packagePath + '/' + glob, packagePath);
