8421f778d40ea9d5fe87a21cd518aa5afea1fd73
franktip <f.tip@northeastern.edu>
2024-03-04 13:46:26 -0500

    report generation

---
+++ .github/generateReport.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+function generateReport(dirName){
+  const report = `
+# ${title}
+Project | # Mutants | # Killed | # Survived | # Timeout |  # Mutation Score | # Time | --:|`;
+  const files = fs.readdirSync(dirName);
+  const benchmarks = files.filter(file => file.endsWith('.json'));
+  const reportData = benchmarks.map(benchmark => {
+    const data = fs.readFileSync(`${dirName}/${StrykerInfo.json}`, 'utf8');
+    const jsonObj = JSON.parse(data);
+    report  += `${benchmark} | ${jsonObj.totalKilled + jsonObj.totalSurvived + jsonObj.totalTimedOut} | ${jsonObj.totalKilled} | ${jsonObj.totalSurvived} | ${jsonObj.totalTimedOut} | ${jsonObj.totalMutationScore} | ${jsonObj.totalTime} |`;
+  });
+  console.log(report);
+}  
+
+
+const dirName = process.argv[2];
+generateReport(dirName);
