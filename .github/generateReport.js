2ba48629d4b7f2300c67f31b89b81bb52918ddad
franktip <f.tip@northeastern.edu>
2024-03-07 16:45:56 -0500

    update reporting

--- .github/generateReport.js
+++ .github/generateReport.js
@@ -1,10 +1,26 @@
 const fs = require('fs');
 
-function generateReport(title, dirName){
-  let report = `# ${title}\n`
-  report += '| Project | #Mutants | #Killed | #Survived | #Timeout | MutationScore | Time |\n';
-  report += '|:--------|:---------|:--------|:----------|----------|---------------|------|\n';
-
+function generateReport(title, dirName, mutantsDirName){
+  let report;
+  if (!mutantsDirName) {
+    report = `# ${title}\n`
+    report += '| Project | #Mutants | #Killed | #Survived | #Timeout | MutationScore | Time |\n';
+    report += '|:--------|:---------|:--------|:----------|----------|---------------|------|\n';
+  } else {
+    const llmData = fs.readFileSync(`${mutantsDirName}/${benchmark}/summary.json`, 'utf8');
+    const llmJsonObj = JSON.parse(llmData);
+    const modelName = llmJsonObj.metaInfo.modelName;
+    const temperature = llmJsonObj.metaInfo.temperature;
+    const maxTokens = llmJsonObj.metaInfo.maxTokens;
+    const templateName = llmJsonObj.metaInfo.templateName || "N/A";
+    report = `# ${title}\n`
+    report += `## Model: ${modelName}\n`;
+    report += `## Temperature: ${temperature}\n`;
+    report += `## Max Tokens: ${maxTokens}\n`;
+    report += `## Template: ${templateName}\n`;
+    report += '| Project | #Prompts | #Mutants | #Killed | #Survived | #Timeout | MutationScore | Time |\n';
+    report += '|:--------|:---------|:---------|:--------|:----------|----------|---------------|------|\n';
+  }  
   const files = fs.readdirSync(dirName);
   for (const benchmark of files) {  
     const data = fs.readFileSync(`${dirName}/${benchmark}/StrykerInfo.json`, 'utf8');
@@ -15,11 +31,18 @@ function generateReport(title, dirName){
     const nrTotal = nrKilled + nrSurvived + nrTimedOut;
     const mutationScore = parseFloat(jsonObj.mutationScore);
     const time = jsonObj.time;
-    report += `| ${benchmark} | ${nrTotal} | ${nrKilled} | ${nrSurvived} | ${nrTimedOut} | ${mutationScore} | ${time} |\n`;
+    if (!mutantsDirName) {
+      report += `| ${benchmark} | ${nrTotal} | ${nrKilled} | ${nrSurvived} | ${nrTimedOut} | ${mutationScore} | ${time} |\n`;
+    } else {
+      const llmData = fs.readFileSync(`${mutantsDirName}/${benchmark}/summary.json`, 'utf8');
+      const llmJsonObj = JSON.parse(llmData);
+      const nrPrompts = parseInt(llmJsonObj.nrSyntacticallyValid);
+      report += `| ${benchmark} | ${nrPrompts} | ${nrTotal} | ${nrKilled} | ${nrSurvived} | ${nrTimedOut} | ${mutationScore} | ${time} |\n`;
+    }
   }
   console.log(report);
 }  
 
 const title = process.argv[2];
 const dirName = process.argv[3];
-generateReport(title, dirName);
+generateReport(title, dirName, mutantsDirName);
